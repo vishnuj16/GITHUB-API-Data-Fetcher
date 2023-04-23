b343ed91386fc0f327b04b1bbc84b43b6a787239
Vishnu J <vishnuj016@gmail.com>
2023-04-23 23:05:35 +0530

    Completed Flask Application- GITHUB DATA FETCHER

--- website/wsgi.py
+++ website/wsgi.py
@@ -1,4 +1,12 @@
 from app.main import app
+import webbrowser
+from threading import Timer
+
+def open_browser():
+    url = "http://localhost:8000"
+    webbrowser.open_new(url)
+
 
 if __name__ == "__main__":
+    Timer(1, open_browser).start()
     app.run(port=8000)
